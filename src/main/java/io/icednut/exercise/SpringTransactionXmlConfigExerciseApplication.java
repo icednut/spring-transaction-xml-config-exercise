0da1263cf033b1b811c088e93fb8925dad1f739e
Wangeun Lee <wangeun.lee@sk.com>
2017-02-15 22:05:27 +0900

    first commit

---
+++ src/main/java/io/icednut/exercise/SpringTransactionXmlConfigExerciseApplication.java
@@ -0,0 +1,10 @@
+package io.icednut.exercise;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringTransactionXmlConfigExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTransactionXmlConfigExerciseApplication.class, args);
+    }
+}
